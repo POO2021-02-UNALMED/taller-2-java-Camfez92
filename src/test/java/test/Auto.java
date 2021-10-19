c75f57d558881f4d4df5ea0f80d28ad5c2f98d4d
Camfez92 <62020207+Camfez92@users.noreply.github.com>
2021-10-18 23:59:19 -0500

    Add files via upload

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,4 +1,4 @@
-//package Ejercicio2;
+//package Ejercicio2Aux;
 package test;
 public class Auto {
 	String modelo;
@@ -19,25 +19,20 @@ public class Auto {
 		return cantidad;
 	}
 	String verificarIntegridad() {
-		boolean original=true;
-		if(motor.registro!=registro) {
-			original=false;
-		}
-		else {
-			for(int i=0;i<asientos.length;i++) {
-				if(asientos[i] instanceof Asiento) {
-					if (asientos[i].registro!=registro) {
-						original=false;
+		if (motor.registro==registro){
+			for (Asiento a:asientos) {
+				if(a.getClass().getSimpleName()=="Asiento") {
+					if (a.registro!=registro){
+						return "Las piezas no son originales";
+					
 					}
 				}
 			}
-		}
-		
-		if(original==true) {
-			return "Auto original";
+			
 		}
 		else {
 			return "Las piezas no son originales";
 		}
+		return "Auto original";}
 	}
-}
+
